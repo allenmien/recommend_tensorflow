c78453b9a6f1bfe0b7c1d2b1c7f2351ff6736a5c
Mark <925283731@qq.com>
2018-05-22 23:40:32 +0800

    first commit

---
+++ general_process.py
@@ -0,0 +1,12 @@
+"""
+by Mark
+"""
+
+
+def produce():
+    for num in range(0, 100):
+        cookie = u"oneone"
+        uid = u"first_blood"
+        user_angent = u"Macintosh Chrome Safari"
+        ip = u"1.1.1.1"
+
