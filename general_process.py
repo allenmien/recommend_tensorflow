21901b2225b63f778de69af65f7c10c72cb6f5ae
Mark <925283731@qq.com>
2018-05-22 23:54:19 +0800

    提交初级小白代码

--- general_process.py
+++ general_process.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 """
 by Mark
 """
@@ -9,4 +10,16 @@ def produce():
         uid = u"first_blood"
         user_angent = u"Macintosh Chrome Safari"
         ip = u"1.1.1.1"
+        video_id = u"898923"
+        topic = u"苹果发布会"
+        order_id = u"0"
+        log_type = u"1"
 
+        final = cookie + u"&" + uid + u"&" + user_angent + u"&" + ip + u"&" \
+                + video_id + u"&" + topic + u"&" + order_id + u"&" + log_type
+
+        print final
+
+
+if __name__ == '__main__':
+    produce()
